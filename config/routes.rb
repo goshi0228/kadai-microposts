bd40e3a8cc81730384e757f1df4a5bccfa962257
goshi.nakamura <goshi.nakamura.0228@gmail.com>
2021-03-17 14:05:46 +0000

    kadai-microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -17,7 +17,13 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :likes
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
-
